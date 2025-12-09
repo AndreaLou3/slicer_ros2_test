929be4351d598114aa263636014152dbd0c94716
Andrea Lou <andrea.lou@berkeley.edu>
2025-12-08 20:40:02 -0800

    fix duplicate

--- PointExtension/PointSubscriberExtension/PointSubscriber/Logic/vtkSlicerPointSubscriberLogic.cxx
+++ PointExtension/PointSubscriberExtension/PointSubscriber/Logic/vtkSlicerPointSubscriberLogic.cxx
@@ -159,7 +159,7 @@ void vtkSlicerPointSubscriberLogic::InitializeSubscriber()
   }
 
   // Create a DoubleArray subscriber for xyz
-  auto sub = rosNode->CreateAndAddSubscriberNode("DoubleArray", "/my_point_topic");
+  auto sub = rosNode->CreateAndAddSubscriberNode("vtkMRMLROS2PublisherDoubleArrayNode", "/my_point_topic");
   if (!sub)
   {
     vtkErrorMacro("Failed to create subscriber!");
@@ -215,7 +215,7 @@ void vtkSlicerPointSubscriberLogic::InitializePublisher()
   }
 
   // Create a DoubleArray publisher for target point coordinates
-  this->TargetPointPublisher = rosNode->CreateAndAddPublisherNode("DoubleArray", "/get_target_point");
+  this->TargetPointPublisher = rosNode->CreateAndAddPublisherNode("vtkMRMLROS2PublisherDoubleArrayNode", "/get_target_point");
   if (!this->TargetPointPublisher)
   {
     vtkErrorMacro("Failed to create publisher!");
